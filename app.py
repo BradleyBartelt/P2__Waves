56c33055503de6f75d3c42ff4963d47175c87d7d
Diegokrenz <42850188+Diegokrenz@users.noreply.github.com>
2021-03-24 09:51:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ Flask(__name__) establishes resources on the filesystem (aka package).
 """
 
 from flask import Flask, render_template
+
+from views.Bradley.app import bradley_bp
+from views.Diego.app import diego_bp
 from views.andrew.app import andrew_bp
-from views.tri2.app import y2021_tri2_bp
-from views.tri3.app import y2021_tri3_bp
 from views.colin.app import colin_bp
 
 app = Flask(__name__)
 #app.register_blueprint(y2021_tri1_bp, url_prefix='/y2021/tri1')
 app.register_blueprint(andrew_bp, url_prefix='/andrew')
-app.register_blueprint(andrew_bp, url_prefix='/andrew')
-app.register_blueprint(y2021_tri2_bp, url_prefix='/y2021/tri2')
-app.register_blueprint(y2021_tri3_bp, url_prefix='/y2021/tri3')
+app.register_blueprint(diego_bp, url_prefix='/diego')
 app.register_blueprint(colin_bp, url_prefix='/colin')
+app.register_blueprint(bradley_bp, url_prefix='/bradley')
 
 
 
@@ -29,4 +29,3 @@ def index():
 if __name__ == "__main__":
     # runs the application on the repl development server
     app.run(debug=True, host='127.0.0.1', port='5000')
-
