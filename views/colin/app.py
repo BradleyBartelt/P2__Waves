4a55ffee51c9012b8067ef62fc1190f9f3ee008a
Colin Szeto <kolinszeto@gmail.com>
2021-04-02 09:59:30 -0700

    copied Mr. M's code to colin blueprint location

--- views/colin/app.py
+++ views/colin/app.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-from views.profile.models import temp_info
+from flask import Blueprint, render_template, request
+# from views.profile.models import temp_info
+from views.colin.fibonacci import Fibonacci
 
 colin_bp = Blueprint('colin_bp', __name__,
                           template_folder='templates',
@@ -9,3 +10,12 @@ colin_bp = Blueprint('colin_bp', __name__,
 def index():
     return render_template('colin/mini_lab.html', active_page='colin')
 
+@colin_bp.route('/fibonacci', methods=["GET", "POST"])
+def fibonacci():
+    if request.form:
+        return render_template("colin/fibonacci.html", fibonacci=Fibonacci(int(request.form.get("series"))), active_page='colin')
+    return render_template("colin/fibonacci.html", fibonacci=Fibonacci(2), active_page='colin')
+
+
+#<tr><th>Username</th></tr>
+#<tr><td><input name="username" type="text" size="20"></td></tr>
