09af22434027e0899f86442817c6be5dc811fb61
Diegokrenz <42850188+Diegokrenz@users.noreply.github.com>
2021-04-07 09:48:14 -0700

    Update app.py

--- views/Diego/app.py
+++ views/Diego/app.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
-# from views.Diego import diego_bp
+
+from views.Diego import diego_bp
 
 
 """
@@ -13,16 +14,15 @@ def index():
 from flask import render_template, request, Blueprint
 # from views.Diego import diego_bp
 
-diego_bp = Blueprint('diego', __name__,
-                     template_folder='templates',
-                     static_folder='static', static_url_path='assets')
+diego_bp = Blueprint('diego_bp', __name__,
+                     template_folder='templates', static_url_path='assets')
 
 
-@diego_bp.route('/',methods = ["GET","POST"])
+@diego_bp.route('/', methods = ["GET","POST"])
 def index():
     if request.form:
         return render_template("dmini_lab.html")
-    return render_template("dminilab.html")
+    return render_template("dmini_lab.html")
 
 @diego_bp.route('/fibonacci', methods=["GET", "POST"])
 def fibonacci():
