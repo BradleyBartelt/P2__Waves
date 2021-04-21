132de6680605ea25e64b91943bf666e8d0e34859
Andrew <azhang1818@gmail.com>
2021-04-20 18:41:32 -0700

    Updated Andrew Bubble Sort Lab

--- views/andrew/app.py
+++ views/andrew/app.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from views.andrew import andrew_bp
-from views.andrew.minilab import Pi
+from views.andrew.algo.minilab import Pi
+from views.andrew.algo.bubble import BubbleSort
 
 
 
@@ -16,7 +17,27 @@ def dataSet():
 
 @andrew_bp.route('/bubbleSort',methods = ["GET","POST"])
 def bubbleSort():
+    arr = []
+    isString = False
     if request.form:
-        return render_template("Buuble_sort.html")
+        string = request.form.get("string")
+        arr = string.split()
+        original = string.split()
+        if(request.form["select"] == "integer"):
+            try:
+                for j in range (0,len(arr)):
+                    arr[j] = int(arr[j])
+                for j in range (0,len(original)):
+                    original[j] = int(original[j])
+                return render_template("Buuble_sort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = original)
+            except ValueError:
+                return render_template("Buuble_sort.html",output_list = "Can Only Contain String or Integer",original_list = "Error")
+        else:
+            try:
+                isString = True
+                return render_template("Buuble_sort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = original)
+            except ValueError:
+                return render_template("Buuble_sort.html",output_list = "Can Only Contain String or Integer",original_list = "Error")
+    return render_template("Buuble_sort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = arr)
 
 
