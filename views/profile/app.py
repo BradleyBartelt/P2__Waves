d817e752b4116acd293dc0ad792da803e27c83d3
Colin Szeto <kolinszeto@gmail.com>
2021-05-20 20:20:17 -0700

    adding in redirect

--- views/profile/app.py
+++ views/profile/app.py
@@ -1,17 +1,24 @@
 from flask import Blueprint, render_template
 from views.profile.models import temp_info
 from flask import Blueprint, redirect, render_template, url_for, Flask
+from flask_login import current_user, LoginManager
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import Form, BooleanField, StringField, PasswordField
 from wtforms.validators import InputRequired,Email,Length
 from model.module import User, db, user_records, func
+from main import app
 from views.andrew import andrew_bp
 from flask_bootstrap import Bootstrap
 
 profile_bp = Blueprint('profile_bp', __name__,
                           template_folder='templates',
                           static_folder='static', static_url_path='assets')
+
+app.secret_key = 'xxxxyyyyyzzzzz'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class LoginForm(FlaskForm):
     username = StringField('username',validators=[InputRequired(), Length(min=0,max=15)])
@@ -23,6 +30,10 @@ class RegisterForm(FlaskForm):
     username = StringField('username',validators=[InputRequired(), Length(min=0,max=15)])
     password = PasswordField('password',validators=[InputRequired(), Length(min=0,max=80)])
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @profile_bp.route('/')
 def index():
     return "Colin Location"
@@ -30,6 +41,8 @@ def index():
 
 @profile_bp.route('/userprofile')
 def user_profile():
+    if current_user.is_authenticated:
+        print(current_user.user)
     return render_template("profile/user_profile.html", list_stories = temp_info.all_stories(), list_post=temp_info.all_post())
 
 @profile_bp.route('/settings')
@@ -56,7 +69,7 @@ def login():
         user = User.query.filter_by(username = form.username.data).first()
         if user:
             if user.password == form.password.data:
-                return '<h1>yay</h1>'
+                return redirect(url_for('profile_bp.user_profile'))
 
         return '<h1>Invalid username or password</h1>'
 
