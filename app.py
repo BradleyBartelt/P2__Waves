00391f24499104511d60bc8a04084fd0496f69b0
Colin Szeto <kolinszeto@gmail.com>
2021-05-26 11:04:23 -0700

    moved the chat html back to the main templates folder

--- app.py
+++ app.py
@@ -70,15 +70,6 @@ def chat():
     else:
         return redirect('/chatIndex')
 
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
-                                                                    data['room'],
-                                                                    data['message']))
-    data['created_at'] = datetime.now().strftime("%d %b, %H:%M")
-    # save_message(data['room'], data['message'], data['username'])
-    socketio.emit('receive_message', data, room=data['room'])
-
 @socketio.on('join_room')
 def handle_join_room_event(data):
     # saving the time that joined the room
