e482e6d2d58ca9847ab1f25d4b3f39424d2c832d
Colin Szeto <kolinszeto@gmail.com>
2021-05-26 15:36:33 -0700

    attempted to add a leave room message, not successful yet

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Flask(__name__) establishes resources on the filesystem (aka package).
 """
 
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, join_room
+from flask_socketio import SocketIO, join_room, leave_room
 from flask_login import current_user, LoginManager, login_user, login_required, logout_user
 import datetime
 
@@ -70,13 +70,6 @@ def chat():
     else:
         return redirect('/chatIndex')
 
-@socketio.on('join_room')
-def handle_join_room_event(data):
-    # saving the time that joined the room
-    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
-    join_room(data['room'])
-    socketio.emit('join_room_announcement', data, room=data['room'])
-
 @socketio.on('send_message')
 def handle_send_message_event(data):
     app.logger.info("{} has joined the room {}".format(data['username'],
@@ -85,6 +78,18 @@ def handle_send_message_event(data):
     # ensure message in corresponding room
     socketio.emit('receive_message', data, room=data['room'])
 
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    # saving the time that joined the room
+    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data, room=data['room'])
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data, room=data['room'])
 
 
 
