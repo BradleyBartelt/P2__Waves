8ec3f6df37a480088d3b1179e77a460ba23eaef1
BradleyBartelt <bradleybartelt@gmail.com>
2021-06-08 17:10:22 -0700

    start of about page

--- model/chat_db.py
+++ model/chat_db.py
@@ -1,42 +1,57 @@
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
 from model.module import UserChat
+import dns
 
-client = MongoClient("mongodb+srv://test:test@cluster0.qboud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# client = MongoClient("mongodb+srv://test:test@cluster0.qboud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+USER = "test"
+PASSWORD = "test"
+
+client = MongoClient(
+    "mongodb+srv://" + USER + ":" + PASSWORD + "@cluster0.qboud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 chat_db = client.get_database("ChatDB")
 users_collection = chat_db.get_collection("users_chat")
+users_info = chat_db.get_collection("users_info")
 
 def save_user(username, email, password):
     password_hash = generate_password_hash(password)
     users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash })
 
+def update_user(user, category,item_of_update):
+    chat_db.inventory.update_one(
+        {"_id": user},
+        {"$set": {category:item_of_update },
+         "$currentDate": {"lastModified": True}})
+
 def get_user(username):
     user_data = users_collection.find_one({'_id': username})
     return UserChat(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
 
+def user_info_create(username, name, bio, link,friend,pfp,posts):
+    users_info.insert_one({'_id': username,
+                           'name': name,
+                           'bio': bio,
+                           'website_link': link,
+                           'friend':friend,
+                           'picture':pfp,
+                           'posts':posts
+                           })
+
+mongo_users = []
 def user_to_frontend():
-    print('Hello')
+    for user in users_collection.find():
+        mongo_users.append(user)
+user_to_frontend()
+
+mongo_userz = []
+def info_to_frontend():
+    for user in users_info.find():
+        mongo_userz.append(user)
+info_to_frontend()
 
 if __name__ == "__main__":
-    """save_user('dnhs','dnhs@gmail.com', 'password' )
-    
-    print("running chat_db")
-
-
-    myquery = { "_id": "testing" }
-
-    mydoc = users_collection.find(myquery)
-
-    for x in mydoc:
-        print(x)
-
-    print("------------------")
-    for x in users_collection.find():
-        print(x)
-    """
-    # save_user('andrew','andrew@gmail.com', 'password' )
-    # save_user('testing','testing@gmail.com', 'password' )
-
-#
-    # print("after creation")
+    #user_info_create("billa", "billy", "My name is billy and my username is billy", "https://github.com/P5-Tacos/P5-Tacos-thrift-store","friend","picture","post")
+    #info_to_frontend()
+    print(mongo_userz)
--- views/profile/app.py
+++ views/profile/app.py
@@ -1,5 +1,8 @@
+import login as login
+from werkzeug.security import check_password_hash
+
 from views.profile.models import temp_info
-from flask import Blueprint, redirect, render_template, url_for, request
+from flask import Blueprint, redirect, render_template, url_for, request, session
 from flask_login import current_user, LoginManager, login_user, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField
@@ -8,6 +11,7 @@ from model.module import User, db, user_records, func
 from main import app
 from views.andrew import andrew_bp
 from flask_bootstrap import Bootstrap
+from model.chat_db import *
 
 
 profile_bp = Blueprint('profile_bp', __name__,
@@ -31,11 +35,44 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=0, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=0, max=80)])
 
+class User:
+    def __init__(self, username):
+        self.username = username
 
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.username
+
+    @staticmethod
+    def check_password(password_hash, password):
+        return check_password_hash(password_hash, password)
+
+
+    @login_manager.user_loader
+    def load_user(username):
+        for user in mongo_users:
+            if user["_id"] == username:
+                u = user
+                if not u:
+                    return None
+            return User(username=u['_id'])
+
+"""
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
+"""
 
 @profile_bp.route('/')
 def index():
@@ -47,9 +84,9 @@ def user_profile():
     if request.form:
         target = request.form["input"]
 
-        for item in user_records:
-            if item["username"] == target:
-                find = {"username": item["username"], "age": 21, "single?": True}
+        for item in mongo_users:
+            if item["_id"] == target:
+                find = {"username": item["_d"], "age": 21, "single?": True}
                 return (render_template("profile/Search Result.html", find=find, exist=True))
 
     return render_template("profile/user_profile.html", list_stories=temp_info.all_stories(),
@@ -65,7 +102,6 @@ def user_setting():
 def user_edit():
     return render_template("profile/edit.html")
 
-
 @profile_bp.route('/login', methods=["GET", "POST"])
 def login():
     # TODO: Make the form accept the User Value, or somehow update the form
@@ -73,11 +109,17 @@ def login():
     # TODO: Make The SQL Database work
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first()
-        if user:
-            if user.password == form.password.data:
-                login_user(user)
-                return redirect(url_for('profile_bp.user_profile'))
+        #user = User.query.filter_by(username = form.username.data).first()
+        for user in mongo_users:
+            if user["_id"] == form.username.data:
+                if user["password"] == form.password.data:
+                    user_obj = User(username = user["_id"])
+                    login_user(user_obj)
+                    return redirect(url_for('profile_bp.user_profile'))
+        #if user:
+            #if user.password == form.password.data:
+                #login_user(user)
+                #return redirect(url_for('profile_bp.user_profile'))
 
         return '<h1>Invalid username or password</h1>'
 
@@ -92,7 +134,7 @@ def searchresult():
     print(user_records)
     if request.form:
         temp_list = []
-        for item in user_records:
+        for item in mongo_users:
             temp_list.append(item)
         """
         target = request.form["input"]
@@ -105,11 +147,11 @@ def searchresult():
         x = 0
         while x < len(target):
             for item in temp_list:
-                if item["username"] == target:
+                if item["_id"] == target:
                     if item not in List:
                         List.append(item)
                     exist = True
-                if target[0:x+1] in item["username"]:
+                if target[0:x+1] in item["_id"]:
                     if item not in List:
                         List.append(item)
                     exist = True
@@ -117,12 +159,48 @@ def searchresult():
             x = x+1
     return (render_template("profile/Search Result.html", List=List, exist=exist))
     return (render_template("profile/Search Result.html", List=List, exist=exist))
-
+previous_search = "None"
 @profile_bp.route('user_display',methods = ["GET","POST"])
 def display():
-    if request.form:
-        name = request.form["befriend"]
-    return render_template("profile/user_display.html",name = name)
+    global previous_search
+    name = "Invalid User"
+    bio = "This user exists in the old database"
+    post = "null user"
+    friend = "null user"
+    ratings = "null user"
+    if request.method == 'POST':
+        form_name = request.form['name']
+        if form_name == "view":
+            name = request.form["username"]
+            print("What's the name" + name)
+            for user in mongo_userz:
+                if user["_id"] == name:
+                    previous_search = name
+                    print(previous_search)
+                    bio = user["bio"]
+                    ratings = 0
+                    friend = len(user["friend"])
+                    post = len(user["posts"])
+            return render_template("profile/user_display.html",name = name, bio = bio, post = post, friend = friend, ratings = ratings)
+
+        if form_name == "add":
+            print("reached")
+            name = previous_search
+            for user in mongo_userz:
+                if user["_id"] == name:
+                    user_current = session["_id"]
+                    for userz in mongo_userz:
+                        if userz["_id"] == user_current:
+                            New_list1 = userz["friend"].append(user["_id"])
+                            New_list2 = user["friend"].append(userz["_id"])
+                            update_user(user_current,"friend",New_list1)
+                            update_user(user,"friend",New_list2)
+                    bio = user["bio"]
+                    ratings = 0
+                    friend = len(user["friend"])
+                    post = len(user["posts"])
+            return render_template("profile/user_display.html",name = name, bio = bio, post = post, friend = friend, ratings = ratings)
+    #return render_template("profile/user_display.html",name = name, bio = bio, post = post, friend = friend, ratings = ratings)
 @profile_bp.route('/signup', methods=["GET", "POST"])
 def signup():
     form = RegisterForm()
@@ -148,7 +226,7 @@ def signup():
 
 @profile_bp.route('/admin')
 def admin():
-    return render_template("profile/admin page.html", user_records=user_records)
+    return render_template("profile/admin page.html", user_records=user_records, mongo_users=mongo_users)
 
 @profile_bp.route("/logout", methods=["GET", "POST"])
 @login_required
