96b30f969f5f18ea21bd23ec7629da96d7924650
Colin Szeto <kolinszeto@gmail.com>
2021-06-14 20:08:14 -0700

    deleted out the socket.io section of the code

--- app.py
+++ app.py
@@ -91,33 +91,6 @@ def chat():
     else:
         return redirect('/chatIndex')
 
-"""
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    app.logger.info("{} has joined the room {}".format(data['username'],
-                                                       data['room'],
-                                                       data['message']))
-    # ensure message in corresponding room
-    socketio.emit('receive_message', data, room=data['room'])
-
-@socketio.on('join_room')
-def handle_join_room_event(data):
-    # saving the time that joined the room
-    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
-    join_room(data['room'])
-    socketio.emit('join_room_announcement', data, room=data['room'])
-
-@socketio.on('leave_room')
-def handle_leave_room_event(data):
-    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
-    leave_room(data['room'])
-    socketio.emit('leave_room_announcement', data, room=data['room'])
-"""
-
-
-"""if __name__ == "__main__":
-    # runs the application on the repl development server
-    socketio.run(app, debug=True, host='127.0.0.1', port='5000')"""
 
 if __name__ == "__main__":
     # runs the application on the repl development server
